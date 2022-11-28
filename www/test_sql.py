9f585f5d81ce99303facf99ac0f703cc7a3cdfdd
Bugui <hzh.banxi@qq.com>
2022-11-28 09:57:48 +0800

    添加 test_sql.py，向数据库写入一行数据测试成功

---
+++ www/test_sql.py
@@ -0,0 +1,15 @@
+import orm
+import asyncio
+from models import User, Blog, Comment
+
+async def test(loop):
+    await orm.create_pool(loop=loop, user='root', password='password', db='awesome')
+    u = User(name='测试', email='test2@qq.com', passwd='12345678', image='about:blank')
+    await u.save()
+    orm.__pool.close()
+    await orm.__pool.wait_closed()
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test(loop))
+    loop.close()
